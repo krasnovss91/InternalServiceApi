3b0bae9b1b2b7821494f7991a37317a59d42e578
krasnovss91 <krasnovstanislav91@gmail.com>
2022-10-13 17:27:55 +0400

    repository

--- src/main/java/com/repository/RepositoryMessage.java
+++ src/main/java/com/repository/RepositoryMessage.java
@@ -1,4 +1,9 @@
 package com.repository;
 
-public interface RepositoryMessage {
+import com.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositoryMessage extends JpaRepository<Message, Long> {
 }
