0690b75e2128c7e54aecd19e0803792902838b36
krasnovss91 <krasnovstanislav91@gmail.com>
2022-10-13 17:38:46 +0400

    db_settings

--- src/main/java/com/controller/MessageController.java
+++ src/main/java/com/controller/MessageController.java
@@ -1,4 +1,12 @@
 package com.controller;
 
+import com.model.Message;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MessageController {
+
+    public void sendMessage(Message message){
+
+    }
 }
