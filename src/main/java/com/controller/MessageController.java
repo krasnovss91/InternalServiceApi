7fd5cd4d2dfc758ff490a69ce18f58daff41a858
krasnovss91 <krasnovstanislav91@gmail.com>
2022-10-13 17:46:57 +0400

    controller_1

--- src/main/java/com/controller/MessageController.java
+++ src/main/java/com/controller/MessageController.java
@@ -1,12 +1,20 @@
 package com.controller;
 
 import com.model.Message;
+import com.repository.RepositoryMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
 
-    public void sendMessage(Message message){
+    @Autowired
+    RepositoryMessage repositoryMessage;
 
+    @PostMapping(value = "/sendMessage")
+    public void sendMessage(Message message){
+        //repositoryMessage. вытащим все сообщения или по id ?
     }
 }
